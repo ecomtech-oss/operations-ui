4261422397b3b8aa305c68ec787bf92cbcd7e823
vlad <vladislav.melnikov.98@gmail.com>
2019-12-16 13:36:01 +0300

    test(button): abstract test suite for button

---
+++ src/Buttons/Iternal/runTestButton.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Logout } from '@samokat/operations-icons';
+
+export const runTestButton = AbstractButtonComponent => {
+  return describe(AbstractButtonComponent.displayName, () => {
+    test('react on event', () => {
+      const clickHanlder = jest.fn();
+      const { getByText } = render(
+        <AbstractButtonComponent onClick={clickHanlder}>
+          Exit
+        </AbstractButtonComponent>,
+      );
+      fireEvent.click(getByText('Exit'));
+      expect(clickHanlder).toHaveBeenCalled();
+    });
+
+    test('if button is disabled dont react on events', () => {
+      const clickHanlder = jest.fn();
+      const { getByText } = render(
+        <AbstractButtonComponent onClick={clickHanlder} disabled={true}>
+          Exit
+        </AbstractButtonComponent>,
+      );
+      fireEvent.click(getByText('Exit'));
+      expect(clickHanlder).toBeCalledTimes(0);
+    });
+
+    test('if buttons is loading dont react on events', () => {
+      const clickHanlder = jest.fn();
+      const { getByText } = render(
+        <AbstractButtonComponent
+          onClick={clickHanlder}
+          loading={true}
+          disabled={false}
+        >
+          Exit
+        </AbstractButtonComponent>,
+      );
+      fireEvent.click(getByText('Exit'));
+      expect(clickHanlder).toBeCalledTimes(0);
+    });
+
+    test('it renders icon', () => {
+      const { container } = render(
+        <AbstractButtonComponent startIcon={Logout}>
+          Exit
+        </AbstractButtonComponent>,
+      );
+      expect(container).toContainHTML('svg');
+    });
+  });
+};
