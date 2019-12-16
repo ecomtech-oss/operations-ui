9b1a24460b128c81a027d1ac3f87520f2c935644
vlad <vladislav.melnikov.98@gmail.com>
2019-12-16 13:37:47 +0300

    style(button): no empty line

--- src/Buttons/PrimaryButton/PrimaryButton.test.tsx
+++ src/Buttons/PrimaryButton/PrimaryButton.test.tsx
@@ -1,5 +1,4 @@
 import { runTestButton } from '../Iternal/runTestButton';
-
 import { PrimaryButton } from './PrimaryButton';
 
 runTestButton(PrimaryButton);
--- src/Buttons/SecondaryButton/SecondaryButton.test.tsx
+++ src/Buttons/SecondaryButton/SecondaryButton.test.tsx
@@ -1,5 +1,4 @@
 import { runTestButton } from '../Iternal/runTestButton';
-
 import { SecondaryButton } from './SecondaryButton';
 
 runTestButton(SecondaryButton);
