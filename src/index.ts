e3b080c67ab62200ff4bb6d202f5672174842611
Igor Kamyshev <igor@kamyshev.me>
2019-12-19 14:23:32 +0300

    fix: add miss export

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as Typography from './Typography';
 import Icon from './Icon';
 import Loader from './Loader';
+import Notifications from './Notifications';
+
 export {
   GhostButton,
   SecondaryButton,
   PrimaryButton,
   RoundButton,
 } from './Buttons';
-export { Typography, Icon, Loader };
+
+export { Typography, Icon, Loader, Notifications };
