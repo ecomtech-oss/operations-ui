0f8962dc8bc25a46ab19c2d566018d8657673acb
vlad <vladislav.melnikov.98@gmail.com>
2019-12-25 14:23:03 +0300

    feat(form): error text component

---
+++ src/Form/ErrorText/ErrorText.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import classNames from 'classnames/bind';
+
+import { Caption } from '../../Typography';
+
+import * as styles from './ErrorText.css';
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  className?: string;
+  children: ReactNode;
+}
+
+export const ErrorText = ({ className, children }: Props) => {
+  return <Caption className={cx('wrapper', className)}>{children}</Caption>;
+};
